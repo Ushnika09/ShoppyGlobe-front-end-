a60a1341146012c275ee8d728cc009e5a9119004
Ushnika09 <karushnika@gmail.com>
2025-09-03 00:07:41 +0530

    Updated backend

--- src/Layout/SignUp.jsx
+++ src/Layout/SignUp.jsx
@@ -19,7 +19,7 @@ export default function Signup() {
     setMessage("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch(`${API_BASE_URL}/api/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
